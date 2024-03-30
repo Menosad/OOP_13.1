e8214045b69585aced3298b89603af7a0092acb1
menosad <draignair@gmail.com>
2024-03-30 18:44:41 +0300

    fis bug

--- src/class_smartphone.py
+++ src/class_smartphone.py
@@ -1,10 +1,14 @@
-from classes import Product
+from src.classes import  Product
 
 
 class Smartphone(Product):
+    performance: float
+    model: str
+    memory: str
+    color: str
 
     def __init__(self, name, description, price, quantity,
-                 performance, model, memory, color):
+                 performance: float, model: str, memory: str, color: str):
         super().__init__(name, description, price, quantity)
         self.performance = performance
         self.model = model
