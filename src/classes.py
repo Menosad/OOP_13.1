e5263816458bdec16054e6a3d1106463899c68d7
menosad <draignair@gmail.com>
2024-03-30 16:48:45 +0300

    added an annotation to the category class

--- src/classes.py
+++ src/classes.py
@@ -2,6 +2,10 @@ class Category:
     """Класс принимающий на вход название категории её описание
      содержит в себе список объектов попадающих в эту категорию"""
 
+    name: str
+    description: str
+    products: list
+
     number_categories = 0
     number_products = 0
 
