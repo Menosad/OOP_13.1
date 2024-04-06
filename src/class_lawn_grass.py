1fb3cfb816d8d15f55af9af76d2eb294520e19d0
menosad <draignair@gmail.com>
2024-04-06 20:02:30 +0300

    added docstring for class LawnGrass

--- src/class_lawn_grass.py
+++ src/class_lawn_grass.py
@@ -2,22 +2,26 @@ from src.classes import Product, Category
 
 
 class LawnGrass(Product):
+    """Класс газонная трава"""
     country_of_origin: str
     germination_period: int
     color: str
 
+    #объект класса Category, создается при инициализации объекта класса с травой
     lawngrass = Category('grass', 'lawn grass', [])
 
     def __init__(self, name, description, price, quantity, country_of_origin: str,
                  germination_period: int, color: str):
+        #вызов метода __init__ у родительского класса, для инициализации общих аргументов
         super().__init__(name, description, price, quantity)
         self.country_of_origin = country_of_origin
         self.germination_period = germination_period
         self.color = color
-
+        #добавления продукта в список продуктов созданной категории
         LawnGrass.lawngrass.products = self
 
     def __add__(self, other):
+        """Метода __add__ который складывает общую стоимость двух товаров класса LawnGrass"""
         if issubclass(other.__class__, LawnGrass):
             self_price = self.price
             other_price = other.price
@@ -26,8 +30,3 @@ class LawnGrass(Product):
             return self_summ + other_summ
         else:
             return TypeError
-
-
-gras1 = LawnGrass('Russian grass', 'good grass', 500, 1000, 'Russian',
-                  2, 'green')
-
