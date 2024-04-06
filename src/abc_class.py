dd0816b9bc92134df0525bcb1bcbce07ebd89050
menosad <draignair@gmail.com>
2024-04-06 20:04:10 +0300

    delete class_order.py

--- src/abc_class.py
+++ src/abc_class.py
@@ -7,10 +7,3 @@ class ABCProduct(ABC):
     @abstractmethod
     def new_product(self, name, description, price, quantity, cat_obj=None):
         pass
-
-
-class AbstractOrder(ABC):
-    """Асбрактный класс для создания класса заказов"""
-    @abstractmethod
-    def products(self):
-        pass
